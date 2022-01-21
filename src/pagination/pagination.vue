9486f0623e5bc5043605002aa4ed916fb7dd4dc6
baozoudashijian <Yhldcahcy521>
2022-01-21 16:43:57 +0800

    上一页 & 下一页

--- src/pagination/pagination.vue
+++ src/pagination/pagination.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="g-pagination">
+    <span class="g-pagination-item"><g-icon icon="arrow-left"></g-icon></span>
     <template v-for="n in paginationArr">
       <template v-if="n === currentPage">
         <span class="g-pagination-item active">{{n}}</span>
@@ -11,10 +12,12 @@
         <span class="g-pagination-item">{{n}}</span>
       </template>
     </template>
+    <span class="g-pagination-item"><g-icon icon="arrow-right"></g-icon></span>
   </div>
 </template>
 
 <script>
+  import Icon from '../icon/icon'
   export default {
     name: "pagination",
     props: {
@@ -26,6 +29,9 @@
         type: [String, Number],
         required: true
       }
+    },
+    components: {
+      'g-icon': Icon
     },
     data() {
       const res = unique([1, this.currentPage - 2, this.currentPage - 1, this.currentPage, this.currentPage + 1, this.currentPage + 2, this.totalPage]).filter(number => number > 0).reduce((previousValue, currentValue, currentIndex, array) => {
@@ -54,6 +60,8 @@
   @import "../var"
 
   .g-pagination
+    display: flex
+    align-items: center
     &-item
       display: inline-flex
       justify-content: center
@@ -70,4 +78,6 @@
         color: $primary
       &.dot
         border: none
+      &:last-child
+        margin-right: 0
 </style>
