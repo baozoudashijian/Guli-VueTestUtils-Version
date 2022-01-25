2c1f77e1cd501354982674abdc77bca0ac3b5278
baozoudashijian <Yhldcahcy521>
2022-01-25 16:09:56 +0800

    style: table需要设置width才能显示，设置display:block不会有效果

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <g-table :dataSource="dataSource" :columns="columns"/>
     <g-pagination :total-page="10" :current-page.sync="currentPage"></g-pagination>
   </div>
 </template>
@@ -7,18 +8,51 @@
 <script>
 
   import Pagination from './pagination/pagination'
+  import Table from './table/table'
 
-export default {
-  name: 'App',
-  data() {
-    return {
-      currentPage: 2
+  export default {
+    name: 'App',
+    data() {
+      return {
+        currentPage: 2,
+        dataSource: [
+          {
+            key: '1',
+            name: '胡彦斌',
+            age: 32,
+            address: '西湖区湖底公园1号',
+          },
+          {
+            key: '2',
+            name: '胡彦祖',
+            age: 42,
+            address: '西湖区湖底公园1号',
+          },
+        ],
+        columns: [
+          {
+            title: '姓名',
+            dataIndex: 'name',
+            key: 'name',
+          },
+          {
+            title: '年龄',
+            dataIndex: 'age',
+            key: 'age',
+          },
+          {
+            title: '住址',
+            dataIndex: 'address',
+            key: 'address',
+          },
+        ]
+      }
+    },
+    components: {
+      'g-pagination': Pagination,
+      'g-table': Table
     }
-  },
-  components: {
-    'g-pagination': Pagination
   }
-}
 </script>
 
 <style lang="scss">
---
+++ src/table/table.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="g-table">
+    <table>
+      <tr>
+        <th v-for="column in columns">{{column.title}}</th>
+      </tr>
+      <tr v-for="dataSourceItem in dataSource">
+        <td v-for="key in Object.keys(dataSourceItem).filter(item => item !== 'key')">{{key}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "table",
+    props: {
+      dataSource: {
+        type: Array,
+        required: true
+      },
+      columns: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped lang="sass">
+  .g-table
+    table
+      /*display: block*/
+      width: 100%
+</style>
